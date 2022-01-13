4d77b21b05df3564a538ec2f58b85f0590c787b7
Alex Soloviev <alex.soloviev@gmail.com>
2022-01-13 01:04:25 -0500

    added databricks and clusters

--- main.tf
+++ main.tf
@@ -1,27 +1,14 @@
-terraform {
-  # required version?
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.89.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-}
-
+# Declare some local variables
 locals {
   build_environment = var.environment == "production" ? "Prod" : "Dev"
   common_tags = {
     Product      = "SAEB Platform"
     ProductOwner = "Shaun Laughland"
-    CreatedBy    = "Ben Larabie"
+    CreatedBy    = "Terraform"
   }
 }
 
+# Create Azure resource group
 resource "azurerm_resource_group" "saeb" {
   name     = "SAEB-DataAnalytics-${local.build_environment}"
   location = var.location 
@@ -33,6 +20,13 @@ resource "azurerm_resource_group" "saeb" {
       BuildVersion = var.terraform_script_version
     }
   )
+}
+
+# Create ADF
+resource "azurerm_data_factory" "saeb_adf" {
+  name                = "adf-saeb-dev-01-${random_string.random.result}"
+  location            = azurerm_resource_group.saeb.location
+  resource_group_name = azurerm_resource_group.saeb.name
 }
 
 resource "random_string" "random" {
@@ -49,14 +43,35 @@ resource "azurerm_storage_account" "saeb_storage" {
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
-  tags = {
-    environment = "staging"
-  }
+  tags = local.common_tags
 }
 
+# Creates all the containers at once
 resource "azurerm_storage_container" "saeb_storage_container" {
   for_each              = toset(var.storage_containers)
   name                  = each.key
   storage_account_name  = azurerm_storage_account.saeb_storage.name
   container_access_type = "private"
+}
+
+# Create Databricks workspace and clusters
+resource "azurerm_databricks_workspace" "saeb_databricks_workspace" {
+  name                = "dbw-saeb-dev-01-${random_string.random.result}"
+  resource_group_name = azurerm_resource_group.saeb.name
+  location            = var.location
+  sku                 = "standard"
+
+  tags = local.common_tags
+}
+
+resource "databricks_cluster" "saeb_databricks_autoscaling" {
+  cluster_name            = "terraform_stnd12"
+  spark_version           = "7.3.x-scala2.12"
+  node_type_id            = "Standard_DS3_v2"
+  # node_type_id            = data.databricks_node_type.smallest.id
+  autotermination_minutes = 20
+  autoscale {
+    min_workers = 0
+    max_workers = 1
+  }
 }
---
+++ providers.tf
@@ -0,0 +1,23 @@
+#TODO: declare backend to specify where to keep state file, provide version number
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.89.0"
+    }
+    databricks = {
+      source = "databrickslabs/databricks"
+      version = "0.4.4"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+provider "databricks" {
+  azure_workspace_resource_id = azurerm_databricks_workspace.saeb_databricks_workspace.id
+} 
